8555c0fcea402c514ed4264a06b6d2116e4a0d8a
tmouhamed <tmouhame@my.centennialcollege.ca>
2024-09-30 11:20:43 -0400

    fixed classes names

--- src/pages/landingPage/landingPage.js
+++ src/pages/landingPage/landingPage.js
@@ -1,25 +1,25 @@
 import React from "react"
 import './landingPage.css'
 import {Link} from "react-scroll"
-import chat from '../../assets/chat.png'
+import resume from '../../assets/chat.png'
 import profilePhoto from '../../assets/profilePhoto.png'
 
 const LandingPage = () => {
     return (
-        <section id="about" className="heroSection">
-            <div className="heroSection-left">
+        <section id="about" className="hero_section">
+            <div className="hero_section_left">
                 <span className='hello'>Hello,</span>
-                <span className="introTitle">I'm Toqa <span className="introName"> Mouhamed</span> <br />Software Engineer</span>
-                <p className="introBody">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type</p>
+                <span className="intro_itle">I'm Toqa <span className="intro_name"> Mouhamed</span> <br />Software Engineer</span>
+                <p className="intro_body">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type</p>
                 <Link>
                     <button className='btn'>
-                        <img className='btnImg' src={chat} alt='chat'/>
-                        <span className='btnText'>Get in Touch</span>
+                        <img className='btn_img' src={resume} alt='chat'/>
+                        <span className='btn_text'>My Resume</span>
                     </button>
                 </Link>
             </div>
-                <div className="heroSection-right">
-                    <img src={profilePhoto} alt="profile" className="profilePhoto" />
+                <div className="hero_section_right">
+                    <img src={profilePhoto} alt="profile" className="profile_photo" />
                 </div>
         </section>
     )
