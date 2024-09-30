0022e6c6e0f68a4dc636fbd5164cccaed6e3a104
tmouhamed <tmouhame@my.centennialcollege.ca>
2024-09-29 21:35:37 -0400

    linked the social links to the icons

--- src/pages/ContactPage/ContactPage.js
+++ src/pages/ContactPage/ContactPage.js
@@ -8,8 +8,8 @@ export const ContactPage = () => {
                 <p className="sub_title">Get In Touch!</p>
                 <h2 className="sub_title">Contact Me</h2>
                 <p>t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a</p>
-                {data?.contacts?.map((item, index) => (
-                    <a> <img src={item.icon} alt={item.}/></a>
+                {data?.contacts?.map((item) => (
+                    <a href={item.link} target="_blank"> <img src={item.icon} alt={item.name}/></a>
                 ))}
             </div>
         </section>
