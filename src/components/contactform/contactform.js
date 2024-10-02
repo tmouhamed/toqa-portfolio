66e87c0a3d172bd5e416e2c4dfbecbc8824391e8
tmouhamed <tmouhame@my.centennialcollege.ca>
2024-10-01 21:34:00 -0400

    added emailjs

--- src/components/contactform/contactform.js
+++ src/components/contactform/contactform.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, {useRef} from 'react'
+import emailjs from '@emailjs/browser'
 import './contactform.css'
-export const Contactform = () => (
-    <form className="contact_form">
-        <input type="text" className="form_name" placeholder="Your Name" name="your_name"/>
-        <input type="email" className="form_email" placeholder="Your Email" name="your_email"/>
-        <textarea className="form_message" rows="5" placeholder="Your Message" name="your_message"/>
-        <button type="submit" value="send" className="form_btn">Submit</button>
-    </form>
-)
+
+export const Contactform = () => {
+    const form = useRef()
+    return(
+        <form className="contact_form" ref={form}>
+            <input type="text" className="form_name" placeholder="Your Name" name="your_name"/>
+            <input type="email" className="form_email" placeholder="Your Email" name="your_email"/>
+            <textarea className="form_message" rows="5" placeholder="Your Message" name="your_message"/>
+            <button type="submit" value="send" className="form_btn">Submit</button>
+        </form>
+        )
+}
