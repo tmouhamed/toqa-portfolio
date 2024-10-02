bb3dd9864e910c96febe69731b37d05e8682f71f
tmouhamed <tmouhame@my.centennialcollege.ca>
2024-10-01 22:02:40 -0400

    fixed the footer

--- src/components/contactform/contactform.js
+++ src/components/contactform/contactform.js
@@ -28,7 +28,7 @@ export const Contactform = () => {
             <input type="text" className="form_name" placeholder="Your Name" name="your_name"/>
             <input type="email" className="form_email" placeholder="Your Email" name="your_email"/>
             <textarea className="form_message" rows="5" placeholder="Your Message" name="message"/>
-            <button type="submit" value="send" className="form_btn" mouse>Submit</button>
+            <button type="submit" value="send" className="form_btn" >Submit</button>
         </form>
         )
 }
