e54ec811820d991f79d181edb4de7e94a6f69c34
tmouhamed <tmouhame@my.centennialcollege.ca>
2024-10-01 20:08:31 -0400

    added about text and css

--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -4,8 +4,10 @@ import './AboutPage.css'
  export const  AboutPage = () => {
      return (
          <section id={"aboutme"} className="about_page">
-             <h1>About Me</h1>
-             <p>hasgwfhghjkskljhjshv</p>
+             <div className={"about_inner"}>
+                 <h1 className="about_inner_title">About</h1>
+                 <p className="about_inner_text">Welcome to my portfolio! I’m a dedicated software engineer currently studying the Software Engineering AI track at Centennial College, building on my background in computer science. With a passion for creating innovative solutions and a keen interest in artificial intelligence, I strive to merge technical expertise with real-world applications. As a proud parent of two, I balance my professional aspirations with family life, constantly inspired by the challenges and joys that come with both roles. I invite you to explore my projects and learn more about my journey in the ever-evolving tech landscape</p>
+             </div>
          </section>
      )
  }
